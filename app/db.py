4c2e407e3d7b81cd7272327f53f62a0d10b0f178
Lucas Freitas Fernandes <lf051188@gmail.com>
2025-09-06 22:58:22 -0300

    Update db.py

--- app/db.py
+++ app/db.py
@@ -6,10 +6,11 @@ def conectar():
         conexao = mysql.connector.connect(
             host="localhost",
             user="root",
-            password="1234",  #  coloque aqui a senha do seu MySQL
-            database="ltp"      # certifique-se de que esse banco existe
+            password="",  #  coloque aqui a senha do seu MySQL
+            database=""      # certifique-se de que esse banco existe
         )
         return conexao
     except mysql.connector.Error as erro:
         print(f"Erro ao conectar ao MySQL: {erro}")
         return None
+
